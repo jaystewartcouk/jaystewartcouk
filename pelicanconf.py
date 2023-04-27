c9ee51f9a0f31a6f1196ea91dc813627c007c9a3
Jay Stewart <jay@jaystewart.co.uk>
2023-04-27 07:04:16 -0600

    Remove blogroll and add social widgets

--- pelicanconf.py
+++ pelicanconf.py
@@ -16,14 +16,15 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+# LINKS = (('Pelican', 'https://getpelican.com/'),
+#          ('Python.org', 'https://www.python.org/'),
+#          ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+#          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('GitHub', 'https://github.com/jaystewartcouk'),
+          ('LinkedIn', 'https://www.linkedin.com/in/jay-stewart/'),
+          ('Twitter', 'https://twitter.com/jaystewartcouk'),)
 
 DEFAULT_PAGINATION = False
 
